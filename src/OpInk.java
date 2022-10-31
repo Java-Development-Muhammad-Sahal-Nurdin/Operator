63f2ecc0102b17d339ba1b82c6fb90104afb5e44
hp <sahalnurdin@gmail.com>
2022-10-31 09:15:00 +0700

    add files

--- src/OpInk.java
+++ src/OpInk.java
@@ -11,10 +11,10 @@ public class OpInk {
         d = a++;
         c++;
 
-        System.out.println("a =" + a);
-        System.out.println("b =" + b);
-        System.out.println("c =" + c);
-        System.out.println("d =" + d);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
 
     }
 
