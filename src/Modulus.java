9f47bf477b2dfc8e00a425f4781a3ba63ddf2acc
hp <sahalnurdin@gmail.com>
2022-10-31 08:46:35 +0700

    add folder

---
+++ src/Modulus.java
@@ -0,0 +1,13 @@
+// Problem: Tunjukkan suatu program yang memuat sebuah operator modulus, % yang menghasilkan sisa dari operasi pembagian. Porgram juga harus bisa di terapkan terhadap tipe titik-mengambang.
+
+public class Modulus {
+    public static void main(String[] args) {
+        int x = 18;
+        double y = 18.7;
+
+        System.out.println("x mod 10 = " + x % 10);
+        System.out.println("x mod 10 = " + y % 10);
+
+    }
+
+}
