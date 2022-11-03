ab3779c960524bf99da3f0888c2ea9f33e5daaf0
hp <sahalnurdin@gmail.com>
2022-11-03 18:54:00 +0700

    changes

--- src/PenggeseranKanan.java
+++ src/PenggeseranKanan.java
@@ -1,5 +1,12 @@
+// Problem: Berikan contoh program untuk mengilustrasikan contoh penggunaan operasi penggeseran kanan.
 public class PenggeseranKanan {
     public static void main(String[] args) {
+        char hex[] = { '0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+        };
+        byte b = (byte) 0xf1;
+
+        System.out.println("b = 0x" + hex[(b >> 4) & 0x0f] + hex[b & 0x0f]);
 
     }
 
