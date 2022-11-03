3b34cb65973ff078e83a95e7750e0ee495c02ead
hp <sahalnurdin@gmail.com>
2022-11-03 18:43:26 +0700

    changes

--- src/GeserKiri.java
+++ src/GeserKiri.java
@@ -1,5 +1,14 @@
+// Lakukan penggeseran kiri sejauh 2 bit terhadap suatu nilai bertipe byte dan berikan program untuk membuktikannya 
 public class GeserKiri {
     public static void main(String[] args) {
+        byte a = 64, b;
+        int i;
+
+        i = a << 2;
+        b = (byte) (a << 2);
+
+        System.out.println("Nilai awal dari a: " + a);
+        System.out.println("i dan b: " + i + " " + b);
 
     }
 
