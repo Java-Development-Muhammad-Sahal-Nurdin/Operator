ebe01793397d77c3b8cd004293600b3000501b73
hp <sahalnurdin@gmail.com>
2022-11-03 18:47:14 +0700

    changes

--- src/PerkalianDua.java
+++ src/PerkalianDua.java
@@ -1,5 +1,13 @@
+// Probelem : Tunjukkan suatu program yang membuktikan bahwa penggeseran bit ke kiri bisa dijadikan cara alternatif dalam mengalikan dengan dua
 public class PerkalianDua {
     public static void main(String[] args) {
+        int i;
+        int angka = 0xFFFFFFE;
+
+        for (i = 0; i < 4; i++) {
+            angka = angka << 1;
+            System.out.println(angka);
+        }
 
     }
 
