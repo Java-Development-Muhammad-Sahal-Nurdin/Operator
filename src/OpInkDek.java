83d2256c4e1dc34b583ddf08d573e136835eaf14
hp <sahalnurdin@gmail.com>
2022-11-03 17:11:42 +0700

    changes

--- src/OpInkDek.java
+++ src/OpInkDek.java
@@ -1,5 +1,28 @@
+// Berikan satu lagi program untuk mengilustrasikan operasi inkremen dan dekremen.
 public class OpInkDek {
     public static void main(String[] args) {
+        char c = 'R';
+        byte j = 127;
+        short k = 32767;
+
+        System.out.println("c = " + c);
+        c++;
+        System.out.println("c = " + c);
+        c++;
+        System.out.println("c = " + c);
+
+        System.out.println("j = " + j);
+        j--;
+        System.out.println("j = " + j);
+        ++j;
+        System.out.println("j = " + j);
+        System.out.println("j = " + j);
+
+        System.out.println("k = " + k);
+        k -= 4;
+        System.out.println("k = " + k);
+        k += 5;
+        System.out.println("k = " + k);
 
     }
 
