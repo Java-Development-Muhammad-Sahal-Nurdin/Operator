e49917e2a8994dbfb4d001b4dc0422ee53433224
hp <sahalnurdin@gmail.com>
2022-11-06 22:08:29 +0700

    add changes

--- src/LogikaBool.java
+++ src/LogikaBool.java
@@ -1,3 +1,4 @@
+// Problem: Berikan suatu program untuk mendemonstrasikan penggunaan tiap operasi logikal pad nilai-nilai boolean.
 public class LogikaBool {
     public static void main(String[] args) {
 
--- src/PenugasanBitwise.java
+++ src/PenugasanBitwise.java
@@ -1,3 +1,4 @@
+// Problem: Ujilah sebuah program yang menciptakan beberapa variabel integer dan kemudian menggunakan beberapa penugasan operator bitwise untuk memanipulasi variabel-variabel tersebut.
 public class PenugasanBitwise {
     public static void main(String[] args) {
 
